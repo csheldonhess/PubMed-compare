d22648378986d2486ac58ea85adf777c8680ddd1
csheldonhess <coral@sheldon-hess.org>
2014-10-16 15:27:55 -0400

    Add PMC_FM for comparison

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Compare PubMed's PMC and DC metadata
 DC: http://www.pubmedcentral.nih.gov/oai/oai.cgi?verb=ListRecords&metadataPrefix=oai_dc&from=2014-10-08
 
 PMC: http://www.pubmedcentral.nih.gov/oai/oai.cgi?verb=ListRecords&metadataPrefix=pmc&from=2014-10-08
+
+PMC_FM: http://www.pubmedcentral.nih.gov/oai/oai.cgi?verb=ListRecords&metadataPrefix=pmc_fm&from=2014-10-13
---
+++ pubmed_pmc_fm.py
@@ -0,0 +1,30 @@
+from lxml import etree
+from datetime import date, timedelta
+import re
+import requests
+import time
+
+
+NAMESPACES = {'ns0': 'http://www.openarchives.org/OAI/2.0/',
+              'pmc': 'http://dtd.nlm.nih.gov/2.0/xsd/archivearticle'}
+
+titles = []
+start_date = date.today() - timedelta(3)
+base_url = 'http://www.pubmedcentral.nih.gov/oai/oai.cgi?verb=ListRecords' 
+oai_pmc_request = base_url + '&metadataPrefix=pmc_fm&from={}'.format(str(start_date)) 
+print(oai_pmc_request)
+data = requests.get(oai_pmc_request)
+doc = etree.XML(data.content)
+records = doc.xpath('//ns0:record', namespaces=NAMESPACES)
+for record in records:
+    r = record.xpath(".//pmc:title-group/pmc:article-title", namespaces=NAMESPACES)
+    if len(r) > 1:
+        raise Exception('more than one title')
+    record_string = etree.tostring(r[0])
+    titles.append(re.sub(r'\<.*?\>', '', record_string).strip())
+
+
+with open('pubmedPMCFM.txt', 'w') as f:
+    for title in titles:
+        f.write(title + '\n')
+    
